657eb61ffce9deefe2bf1c2563b3a36231e6842d
SAR-KUMAR <88149210+SAR-KUMAR@users.noreply.github.com>
2022-04-28 17:40:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+
+    });
+}
+
+console.log("ml5 version",ml5.version);
+
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_x1dAdbbf/model.jason",modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelloaded")
+}
